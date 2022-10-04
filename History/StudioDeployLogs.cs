c032bdf736500070b29c4bc519dca1c228844d5c
Max <clonet1019@gmail.com>
2022-10-04 12:37:54 -0500

    Don't bother with history for hidden logs.

--- History/StudioDeployLogs.cs
+++ History/StudioDeployLogs.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Net;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -15,6 +14,7 @@ namespace RobloxDeployHistory
         private static readonly NumberFormatInfo NumberFormat = NumberFormatInfo.InvariantInfo;
 
         public Channel Channel { get; private set; }
+        public bool HasHistory { get; private set; }
 
         private string LastDeployHistory = "";
         private static readonly Dictionary<string, StudioDeployLogs> LogCache = new Dictionary<string, StudioDeployLogs>();
@@ -71,76 +71,82 @@ namespace RobloxDeployHistory
                 Is64Bit = binaryType.EndsWith("64", StringFormat),
                 VersionGuid = info.VersionGuid,
                 TimeStamp = DateTime.Now,
+                Channel = channel,
 
                 MajorRev = versionData[0],
                 Version = versionData[1],
                 Patch = versionData[2],
                 Changelist = versionData[3],
-
-                Channel = channel,
-                GitHash = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
             };
         }
 
         private async Task UpdateLogs(Channel channel, string deployHistory)
         {
-            var now = DateTime.Now;
-            var matches = Regex.Matches(deployHistory, LogPattern);
-
             CurrentLogs_x86.Clear();
             CurrentLogs_x64.Clear();
 
-            foreach (Match match in matches)
+            if (deployHistory.Contains("version-hidden"))
             {
-                string[] data = match.Groups.Cast<Group>()
-                    .Select(group => group.Value)
-                    .Where(value => value.Length != 0)
-                    .ToArray();
-
-                string buildType = data[1];
-                string versionGuid = data[2];
-
-                if (versionGuid.ToLowerInvariant() == "version-hidden")
-                    continue;
-
-                var deployLog = new DeployLog()
+                if (Environment.Is64BitOperatingSystem)
                 {
-                    VersionGuid = versionGuid,
-                    TimeStamp = DateTime.Parse(data[3], DateTimeFormatInfo.InvariantInfo),
-
-                    MajorRev = int.Parse(data[4], NumberFormat),
-                    Version = int.Parse(data[5], NumberFormat),
-                    Patch = int.Parse(data[6], NumberFormat),
-                    Changelist = int.Parse(data[7], NumberFormat),
-
-                    Is64Bit = buildType.EndsWith("64", StringFormat),
-                    Channel = channel,
-                    GitHash = data[8]
-                };
-
-                HashSet<DeployLog> targetList;
-
-                if (deployLog.Is64Bit)
-                    targetList = CurrentLogs_x64;
+                    var liveInfo_x64 = await GetLiveLog(channel, "WindowsStudio64");
+                    CurrentLogs_x64.Add(liveInfo_x64);
+                }
                 else
-                    targetList = CurrentLogs_x86;
+                {
+                    var liveInfo_x86 = await GetLiveLog(channel, "WindowsStudio");
+                    CurrentLogs_x86.Add(liveInfo_x86);
+                }
 
-                targetList.Add(deployLog);
-            }
-
-            if (Environment.Is64BitOperatingSystem)
-            {
-                var liveInfo_x64 = await GetLiveLog(channel, "WindowsStudio64");
-                CurrentLogs_x64.Add(liveInfo_x64);
+                HasHistory = false;
             }
             else
             {
-                var liveInfo_x86 = await GetLiveLog(channel, "WindowsStudio");
-                CurrentLogs_x86.Add(liveInfo_x86);
-            }
+                var matches = Regex.Matches(deployHistory, LogPattern);
 
-            MakeDistinct(CurrentLogs_x64);
-            MakeDistinct(CurrentLogs_x86);
+                foreach (Match match in matches)
+                {
+                    string[] data = match.Groups.Cast<Group>()
+                        .Select(group => group.Value)
+                        .Where(value => value.Length != 0)
+                        .ToArray();
+
+                    string buildType = data[1];
+                    string versionGuid = data[2];
+
+                    if (versionGuid.ToLowerInvariant() == "version-hidden")
+                        continue;
+
+                    var deployLog = new DeployLog()
+                    {
+                        VersionGuid = versionGuid,
+                        TimeStamp = DateTime.Parse(data[3], DateTimeFormatInfo.InvariantInfo),
+
+                        MajorRev = int.Parse(data[4], NumberFormat),
+                        Version = int.Parse(data[5], NumberFormat),
+                        Patch = int.Parse(data[6], NumberFormat),
+                        Changelist = int.Parse(data[7], NumberFormat),
+
+                        Is64Bit = buildType.EndsWith("64", StringFormat),
+                        Channel = channel,
+                        GitHash = data[8]
+                    };
+
+                    HashSet<DeployLog> targetList;
+
+                    if (deployLog.Is64Bit)
+                        targetList = CurrentLogs_x64;
+                    else
+                        targetList = CurrentLogs_x86;
+
+                    targetList.Add(deployLog);
+                }
+
+                MakeDistinct(CurrentLogs_x64);
+                MakeDistinct(CurrentLogs_x86);
+
+                HasHistory = true;
+            }
         }
 
         public static async Task<StudioDeployLogs> Get(Channel channel)
@@ -152,8 +158,7 @@ namespace RobloxDeployHistory
             else
                 logs = new StudioDeployLogs(channel);
 
-            var getDeployHistory = HistoryCache.GetDeployHistory(channel);
-            string deployHistory = await getDeployHistory.ConfigureAwait(false);
+            string deployHistory = await HistoryCache.GetDeployHistory(channel);
 
             if (logs.LastDeployHistory != deployHistory)
             {
